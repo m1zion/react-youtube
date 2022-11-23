89966dd3d1016132164d0972212746e035e494a1
unknown <misaelzion@gmail.com>
2022-11-22 18:39:48 -0600

    class 2

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Regular const";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       Hello <h1>{ name }</h1>
+       {/*
+       we call the component
+       we pass the params as strings or as numbers {}
+       we get the params as props in the component
+       */}
+       <GetNameComponent name="Jessica" age={21} email="jessica@mail.com"/>
     </div>
   );
 }
 
+
+const getName = () =>{
+  return "Name";
+}
+//COMPONENTS
+//Starts with a capital letter
+//Returns HTML codde
+//if we return more than one line we have to use ()
+const GetNameComponent = (props) =>{
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+}
 export default App;
