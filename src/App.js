7fbbe39de57dba16088c2702ad904e71aebb2e1f
misael <misaelzion@gmail.com>
2022-11-26 19:25:17 -0600

    clase 3 cssd and lists

--- src/App.js
+++ src/App.js
@@ -3,21 +3,51 @@ import './App.css';
 
 function App() {
   const name = "Regular const";
+  const isGreen = true;
+
+  const names = ["peter","jake","Jessica","Dustin"];
+
+  const users= [
+    {name:"Pedro", age:21},
+    {name:"Jake", age:25},
+    {name:"Jessica", age:45}
+  ];
+
 
   return (
     <div className="App">
-       Hello <h1>{ name }</h1>
+       Hello <h1 className='name'>{ name }</h1>
        {/*
        we call the component
        we pass the params as strings or as numbers {}
        we get the params as props in the component
        */}
+       <h1 style={{color: isGreen ? "green": "red"}}>This is a color</h1>
        <GetNameComponent name="Jessica" age={21} email="jessica@mail.com"/>
+
+       {
+        names.map((name, key) => {
+            return <h1 key={key}>{name}</h1>
+        })
+       }
+
+      <h1>CLASS 3 LIST</h1>
+      {
+        users.map((user, key) => (
+            <User  key={key} name={user.name} age={user.age}/>
+            ))
+       }
+       <h1></h1>
     </div>
   );
 }
 
+const User = (props) =>{
+  return (
+    <div>{props.name} {props.age}</div>
+  )
 
+}
 const getName = () =>{
   return "Name";
 }
